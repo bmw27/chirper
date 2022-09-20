8e4b01903516d15202a2c40e362e0944dbd11f61
Brandon Williams <bmwill@outlook.com>
2022-09-20 16:01:11 -0700

    finished Chapter 5: Editing Chirps

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,17 +16,23 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+Route::get("/", function () {
+    return Inertia::render("Welcome", [
+        "canLogin" => Route::has("login"),
+        "canRegister" => Route::has("register"),
+        "laravelVersion" => Application::VERSION,
+        "phpVersion" => PHP_VERSION,
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get("/dashboard", function () {
+    return Inertia::render("Dashboard");
+})
+    ->middleware(["auth", "verified"])
+    ->name("dashboard");
 
-require __DIR__.'/auth.php';
+Route::resource("chirps", ChirpController::class)
+    ->only(["index", "store", "update"])
+    ->middleware(["auth", "verified"]);
+
+require __DIR__ . "/auth.php";
